aacec35edf2897a827fd0d92f6116da0bb29d001
SergeyTushinsky <sergeytushinsky@mail.ru>
2024-04-17 21:16:42 +0300

    Добавлен метод solution в класс Conversion

---
+++ src/Conversion.java
@@ -0,0 +1,17 @@
+public class Conversion {
+    /*
+    Symbol    Value
+    I          1
+    V          5
+    X          10
+    L          50
+    C          100
+    D          500
+    M          1,000
+     */
+    public String solution(int n) {
+        // преобразуем число в строку или в массив символов
+        char[] chars = String.valueOf(n).toCharArray();
+        return "";
+    }
+}
